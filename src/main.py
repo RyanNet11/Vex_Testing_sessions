b8c529dec8013ab6f9b7a87de12c782f1c18d5d7
RyanNet11 n <ryangleason07@gmail.com>
2024-05-03 19:35:48 +0000

    FIrst Commit - Initializing formatting

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       gitpod                                                       #
+# 	Created:      5/3/2024, 7:32:08 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
